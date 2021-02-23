077169ae4768c549823ba5925eafef54c2aa90a2
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2021-02-22 22:14:50 -0500

    video playing

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ menu.addEventListener('click',() => {
 
 
 // end of navbar
+
+// Section 2 video
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons i')
+const bar = document. querySelector('.video-bar')
+
+// play/pause
+const playPause = () => {
+    if(video.paused){
+        video.play()
+        btn.className = 'far fa-pause-circle'
+        video.style.opacity = .8
+    }else{
+        video.pause()
+        btn.className = 'far fa-play-circle'
+        video.style.opacity = .3
+    }
+    }
+
+btn.addEventListener('click', () => {
+    playPause()
+})
+
+// duration bar
+video.addEventListener('timeupdate', () =>{
+    // console.log(video.currentTime, video.duration)
+    const barWidth = video.currentTime/video.duration
+    bar.style.width = `${barWidth * 100}%`
+})
+
+// End of Section 2 video
